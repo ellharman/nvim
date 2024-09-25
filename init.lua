08b0e8d8191fe3b98fe7f3fe48ca0afbb7ed0a4c
Ell Harman <ell.harman@strideup.co>
2024-09-25 17:41:18 +0100

    Disable arrow keys in insert mode

--- init.lua
+++ init.lua
@@ -53,6 +53,11 @@ vim.cmd("noremap <Left> <Nop>")
 vim.cmd("noremap <Right> <Nop>")
 vim.cmd("noremap <Up> <Nop>")
 vim.cmd("noremap <Down> <Nop>")
+vim.cmd("inoremap <Left> <Nop>")
+vim.cmd("inoremap <Right> <Nop>")
+vim.cmd("inoremap <Up> <Nop>")
+vim.cmd("inoremap <Down> <Nop>")
+
 
 -- Lualine config
 require("lualine").setup({
